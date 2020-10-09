4af8a077951178abb5dc2be1031a476080842385
Iuliia Kirillova <yvkirillova@yandex.ru>
2020-10-09 16:04:35 +0300

    Final

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,7 @@ export class Section {
 
         this._items = items; 
         this._renderer = renderer;
-        this._container = document.querySelector('.elements');
+        this._container = document.querySelector(containerSelector);
 
     }
 
